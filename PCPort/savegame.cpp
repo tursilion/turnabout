c37f3dab179a7ef4b1e183ef64b9f8d58a5307dd
Tursilion <tursi@harmlesslion.com>
2025-10-14 02:19:09 -0600

    - add savegame

---
+++ PCPort/savegame.cpp
@@ -0,0 +1,6 @@
+// TODO: Determine how to save a game
+#include <conio.h>
+
+void savegame() {
+    cputsxy(0,23,"Save not implemented");
+}
---
+++ PCPort/savegame.h
@@ -0,0 +1,16 @@
+#ifndef SAVEGAME_H
+#define SAVEGAME_H
+
+void savegame();
+
+// Save data VDP addresses (bytes)
+
+/*
+
+3880    F18A flag
+
+
+
+*/
+
+#endif
